9f7205212726e38fddfccbfa2e0caae2ef3de130
Dinesh Lal <dineshlal29@gmail.com>
2022-10-30 17:01:18 +0530

    updated NavBar routes

---
+++ src/Nav.js
@@ -0,0 +1,149 @@
+import React, { Component } from 'react'
+import { Navbar, Link, Text, Avatar, Dropdown } from "@nextui-org/react";
+import { Layout } from "./Layout.js";
+import { AcmeLogo } from "./AcmeLogo.js";
+import { Button } from "@nextui-org/react";
+import {
+    NavLink 
+  } from "react-router-dom";
+
+  const collapseItems = [
+    "Profile",
+    "Dashboard",
+    "Activity",
+    "Analytics",
+    "System",
+    "Deployments",
+    "My Settings",
+    "Team Settings",
+    "Help & Feedback",
+    "Log Out",
+  ];
+
+export default class Navapp extends Component {
+  render() {
+    return (
+        <><Layout>
+        <Navbar isBordered variant="floating">
+            <Navbar.Toggle showIn="xs" />
+            <Navbar.Brand
+            css={{
+                "@xs": {
+                w: "12%"
+                },
+            }}
+            >
+            <AcmeLogo />
+            <Text b css={{"color":"#0a1416"}} hideIn="xs">
+                Andamane
+            </Text>
+            </Navbar.Brand>
+            <Navbar.Content
+            enableCursorHighlight
+            activeColor="secondary"
+            hideIn="xs"
+            variant="highlight-rounded"
+            >
+            <NavLink to="/Explore">
+              <Navbar.Link> 
+                    Explore
+              </Navbar.Link></NavLink>
+              <NavLink to="/Stay"><Navbar.Link isActive >
+                    Stay
+            </Navbar.Link></NavLink>
+            <NavLink to="/Fun">
+                <Navbar.Link >Fun</Navbar.Link>
+                </NavLink>
+            </Navbar.Content>
+            <Navbar.Content
+            css={{
+                "@xs": {
+                w: "12%",
+                jc: "flex-end",
+                },
+            }}
+            >
+            <Dropdown placement="bottom-right">
+                <Navbar.Item>
+                <Dropdown.Trigger>
+                    <Avatar
+                    bordered
+                    as="button"
+                    color="secondary"
+                    size="md"
+                    src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
+                    />
+                </Dropdown.Trigger>
+                </Navbar.Item>
+                <Dropdown.Menu
+                aria-label="User menu actions"
+                color="secondary"
+                onAction={(actionKey) => console.log({ actionKey })}
+                >
+                <Dropdown.Item key="profile" css={{ height: "$18" }}>
+                    <Text b color="inherit" css={{ d: "flex" }}>
+                    Signed in as
+                    </Text>
+                    <Text b color="inherit" css={{ d: "flex" }}>
+                    zoey@example.com
+                    </Text>
+                </Dropdown.Item>
+                <Dropdown.Item key="settings" withDivider>
+                    My Settings
+                </Dropdown.Item>
+                <Dropdown.Item key="team_settings">Team Settings</Dropdown.Item>
+                <Dropdown.Item key="analytics" withDivider>
+                    Analytics
+                </Dropdown.Item>
+                <Dropdown.Item key="system">System</Dropdown.Item>
+                <Dropdown.Item key="configurations">Configurations</Dropdown.Item>
+                <Dropdown.Item key="help_and_feedback" withDivider>
+                    Help & Feedback
+                </Dropdown.Item>
+                <Dropdown.Item key="logout" withDivider color="error">
+                    Log Out
+                </Dropdown.Item>
+                </Dropdown.Menu>
+            </Dropdown>
+            </Navbar.Content>
+            <Navbar.Collapse>
+            {collapseItems.map((item, index) => (
+                <Navbar.CollapseItem
+                key={item}
+                activeColor="secondary"
+                css={{
+                    color: index === collapseItems.length - 1 ? "$error" : "",
+                }}
+                isActive={index === 2}
+                >
+                <Link
+                    color="inherit"
+                    css={{
+                    minWidth: "100%",
+                    }}
+                    href="#"
+                >
+                    {item}
+                </Link>
+                </Navbar.CollapseItem>
+            ))}
+            </Navbar.Collapse>
+        </Navbar>
+
+        <Button.Group color="gradient" ghost css={{"display":"flex","alignContent":"center","justifyContent":"center"}}>
+            <Button>Flights</Button>
+            <Button>Ship</Button>
+            <Button>Virtual Hotel</Button>
+            <Button>Bus</Button>
+            <Button>Sea Activity</Button>
+            <Button>Paying Guest</Button>
+            <Button>Travel Package</Button>
+            <Button>Rental Cab</Button>
+            <Button>Cruiser Dinning</Button>
+            <Button>Trekking</Button>
+        </Button.Group>
+                            
+        </Layout></>
+    )
+  }
+}
---
+++ src/NavMenu/Fun.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class Fun extends Component {
+  render() {
+    return (
+      <div>Fun</div>
+    )
+  }
+}
