3b23f1588d167830a936282b391e227e1e74c66e
unknown <mvvinod777@gmail.com>
2022-11-03 18:20:10 +0530

    card in trekking commit

--- src/ServiceMenu/trekking.js
+++ src/ServiceMenu/trekking.js
@@ -1,27 +1,27 @@
-import { Grid } from "@nextui-org/react";
-
-import  Card  from '../Component/Card';
+import  Card  from '@coreui/react';
 
 
 export default function trekking() {
   
   return (
-    <Grid.Container gap={2} justify="flex-start">
-      <Grid xs={7} sm={3}>
-        <Card />
-      </Grid>
-      <Grid xs={7} sm={3}>
-        <Card />
-      </Grid>
-      <Grid xs={7} sm={3}>
-        <Card />
-      </Grid>
-      <Grid xs={7} sm={3}>
-        <Card />
-      </Grid>
-      <Grid xs={7} sm={3}>
-        <Card />
-      </Grid>
-    </Grid.Container>
+    <CCard className="mb-3" style={{ maxWidth: '540px' }}>
+  <CRow className="g-0">
+    <CCol md={4}>
+      <CCardImage src="/images/react400.jpg" />
+    </CCol>
+    <CCol md={8}>
+      <CCardBody>
+        <CCardTitle>Card title</CCardTitle>
+        <CCardText>
+          This is a wider card with supporting text below as a natural lead-in to additional
+          content. This content is a little bit longer.
+        </CCardText>
+        <CCardText>
+          <small className="text-medium-emphasis">Last updated 3 mins ago</small>
+        </CCardText>
+      </CCardBody>
+    </CCol>
+  </CRow>
+</CCard>
   );
 }
