3401530876c33a440ae8debc530e1a371c906501
unknown <mvvinod777@gmail.com>
2022-11-06 23:03:36 +0530

    hotels updated

--- src/ServiceMenu/dinning.js
+++ src/ServiceMenu/dinning.js
@@ -7,6 +7,7 @@ import Typography from '@mui/material/Typography';
 export default function dining() {
   
   return (
+    <div>
     <Card sx={{ display: 'flex' }}>
       <Box sx={{ display: 'flex', flexDirection: 'column' }}>
           <CardMedia
@@ -18,14 +19,256 @@ export default function dining() {
           />
         <CardContent sx={{ flex: '1 0 auto' }}>
           <Typography component="div" variant="h5">
-            Live From
+            Love Garden
           </Typography>
           <Typography variant="subtitle1" color="text.secondary" component="div">
-            Vinod
+            
           </Typography>
         </CardContent>
         
       </Box>
     </Card>
+    <Card sx={{ display: 'flex' }}>
+      <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+          <CardMedia
+            component="img"
+            image="https://i.pinimg.com/736x/2b/b3/d4/2bb3d46f16ee6abf842845949003857f.jpg"
+            alt="Live from space album cover"
+            className="img-responsive card-img-top"
+            style={{ width: "auto", height: 179 }}
+          />
+        <CardContent sx={{ flex: '1 0 auto' }}>
+          <Typography component="div" variant="h5">
+            Hotel Mansha Regency
+          </Typography>
+          <Typography variant="subtitle1" color="text.secondary" component="div">
+            
+          </Typography>
+        </CardContent>
+        
+      </Box>
+    </Card>
+    <Card sx={{ display: 'flex' }}>
+      <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+          <CardMedia
+            component="img"
+            image="https://i.pinimg.com/736x/2b/b3/d4/2bb3d46f16ee6abf842845949003857f.jpg"
+            alt="Live from space album cover"
+            className="img-responsive card-img-top"
+            style={{ width: "auto", height: 179 }}
+          />
+        <CardContent sx={{ flex: '1 0 auto' }}>
+          <Typography component="div" variant="h5">
+          Hotel Silver Spring
+          </Typography>
+          <Typography variant="subtitle1" color="text.secondary" component="div">
+            
+          </Typography>
+        </CardContent>
+        
+      </Box>
+    </Card>
+    <Card sx={{ display: 'flex' }}>
+      <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+          <CardMedia
+            component="img"
+            image="https://i.pinimg.com/736x/2b/b3/d4/2bb3d46f16ee6abf842845949003857f.jpg"
+            alt="Live from space album cover"
+            className="img-responsive card-img-top"
+            style={{ width: "auto", height: 179 }}
+          />
+        <CardContent sx={{ flex: '1 0 auto' }}>
+          <Typography component="div" variant="h5">
+            Hotel Ram Nivas
+          </Typography>
+          <Typography variant="subtitle1" color="text.secondary" component="div">
+        
+          </Typography>
+        </CardContent>
+        
+      </Box>
+    </Card>
+    <Card sx={{ display: 'flex' }}>
+      <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+          <CardMedia
+            component="img"
+            image="https://i.pinimg.com/736x/2b/b3/d4/2bb3d46f16ee6abf842845949003857f.jpg"
+            alt="Live from space album cover"
+            className="img-responsive card-img-top"
+            style={{ width: "auto", height: 179 }}
+          />
+        <CardContent sx={{ flex: '1 0 auto' }}>
+          <Typography component="div" variant="h5">
+            Sugar N Spice
+          </Typography>
+          <Typography variant="subtitle1" color="text.secondary" component="div">
+    
+          </Typography>
+        </CardContent>
+        
+      </Box>
+    </Card>
+    <Card sx={{ display: 'flex' }}>
+      <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+          <CardMedia
+            component="img"
+            image="https://i.pinimg.com/736x/2b/b3/d4/2bb3d46f16ee6abf842845949003857f.jpg"
+            alt="Live from space album cover"
+            className="img-responsive card-img-top"
+            style={{ width: "auto", height: 179 }}
+          />
+        <CardContent sx={{ flex: '1 0 auto' }}>
+          <Typography component="div" variant="h5">
+            Cardamom
+          </Typography>
+          <Typography variant="subtitle1" color="text.secondary" component="div">
+            
+          </Typography>
+        </CardContent>
+        
+      </Box>
+    </Card>
+    <Card sx={{ display: 'flex' }}>
+      <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+          <CardMedia
+            component="img"
+            image="https://i.pinimg.com/736x/2b/b3/d4/2bb3d46f16ee6abf842845949003857f.jpg"
+            alt="Live from space album cover"
+            className="img-responsive card-img-top"
+            style={{ width: "auto", height: 179 }}
+          />
+        <CardContent sx={{ flex: '1 0 auto' }}>
+          <Typography component="div" variant="h5">
+            Chai Sutta Bar
+          </Typography>
+          <Typography variant="subtitle1" color="text.secondary" component="div">
+            
+          </Typography>
+        </CardContent>
+        
+      </Box>
+    </Card>
+    <Card sx={{ display: 'flex' }}>
+      <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+          <CardMedia
+            component="img"
+            image="https://i.pinimg.com/736x/2b/b3/d4/2bb3d46f16ee6abf842845949003857f.jpg"
+            alt="Live from space album cover"
+            className="img-responsive card-img-top"
+            style={{ width: "auto", height: 179 }}
+          />
+        <CardContent sx={{ flex: '1 0 auto' }}>
+          <Typography component="div" variant="h5">
+            Hotel Shompen
+          </Typography>
+          <Typography variant="subtitle1" color="text.secondary" component="div">
+            
+          </Typography>
+        </CardContent>
+        
+      </Box>
+    </Card>
+    <Card sx={{ display: 'flex' }}>
+      <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+          <CardMedia
+            component="img"
+            image="https://i.pinimg.com/736x/2b/b3/d4/2bb3d46f16ee6abf842845949003857f.jpg"
+            alt="Live from space album cover"
+            className="img-responsive card-img-top"
+            style={{ width: "auto", height: 179 }}
+          />
+        <CardContent sx={{ flex: '1 0 auto' }}>
+          <Typography component="div" variant="h5">
+            Anumod Restaurant
+          </Typography>
+          <Typography variant="subtitle1" color="text.secondary" component="div">
+            
+          </Typography>
+        </CardContent>
+        
+      </Box>
+    </Card>
+    <Card sx={{ display: 'flex' }}>
+      <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+          <CardMedia
+            component="img"
+            image="https://i.pinimg.com/736x/2b/b3/d4/2bb3d46f16ee6abf842845949003857f.jpg"
+            alt="Live from space album cover"
+            className="img-responsive card-img-top"
+            style={{ width: "auto", height: 179 }}
+          />
+        <CardContent sx={{ flex: '1 0 auto' }}>
+          <Typography component="div" variant="h5">
+            The North Reef
+          </Typography>
+          <Typography variant="subtitle1" color="text.secondary" component="div">
+            
+          </Typography>
+        </CardContent>
+        
+      </Box>
+    </Card>
+    <Card sx={{ display: 'flex' }}>
+      <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+          <CardMedia
+            component="img"
+            image="https://i.pinimg.com/736x/2b/b3/d4/2bb3d46f16ee6abf842845949003857f.jpg"
+            alt="Live from space album cover"
+            className="img-responsive card-img-top"
+            style={{ width: "auto", height: 179 }}
+          />
+        <CardContent sx={{ flex: '1 0 auto' }}>
+          <Typography component="div" variant="h5">
+            Roof Top Cafe
+          </Typography>
+          <Typography variant="subtitle1" color="text.secondary" component="div">
+            
+          </Typography>
+        </CardContent>
+        
+      </Box>
+    </Card>
+    <Card sx={{ display: 'flex' }}>
+      <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+          <CardMedia
+            component="img"
+            image="https://i.pinimg.com/736x/2b/b3/d4/2bb3d46f16ee6abf842845949003857f.jpg"
+            alt="Live from space album cover"
+            className="img-responsive card-img-top"
+            style={{ width: "auto", height: 179 }}
+          />
+        <CardContent sx={{ flex: '1 0 auto' }}>
+          <Typography component="div" variant="h5">
+            Kalki
+          </Typography>
+          <Typography variant="subtitle1" color="text.secondary" component="div">
+            
+          </Typography>
+        </CardContent>
+        
+      </Box>
+    </Card>
+    <Card sx={{ display: 'flex' }}>
+      <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+          <CardMedia
+            component="img"
+            image="https://i.pinimg.com/736x/2b/b3/d4/2bb3d46f16ee6abf842845949003857f.jpg"
+            alt="Live from space album cover"
+            className="img-responsive card-img-top"
+            style={{ width: "auto", height: 179 }}
+          />
+        <CardContent sx={{ flex: '1 0 auto' }}>
+          <Typography component="div" variant="h5">
+            Desi Lounge
+          </Typography>
+          <Typography variant="subtitle1" color="text.secondary" component="div">
+            
+          </Typography>
+        </CardContent>
+        
+      </Box>
+    </Card>
+
+    </div>
   );
 }
