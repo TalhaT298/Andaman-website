cda6126bb3d9818939401b366d7ad383212f1d23
unknown <mvvinod777@gmail.com>
2022-11-18 13:11:45 +0530

    trekking data update

--- src/Data/TrekkingData.js
+++ src/Data/TrekkingData.js
@@ -3,61 +3,61 @@ const datas = [
     {
       id: 1,
       imageLink:
-        "https://firebasestorage.googleapis.com/v0/b/tourismweb-3e2b8.appspot.com/o/havelocksingle.jpeg?alt=media&token=36bb5381-e237-4851-a360-67f8183ea206",
+        "https://res.cloudinary.com/trop101/image/upload/v1589091343/new%20images/mount-harriet-1.jpg ",
      
      
-      name: "Magical Havelock ",
-      duration: "5 Nights / 6 Days",
+      name: "Mount Manipur",
+      duration: "Hope Town, Port Blair",
       description: "Port Blair – 3 Nights , Havelock – 2 Nights ",
     },
     {
         id: 2,
         imageLink:
-          "https://firebasestorage.googleapis.com/v0/b/tourismweb-3e2b8.appspot.com/o/havelocksingle.jpeg?alt=media&token=36bb5381-e237-4851-a360-67f8183ea206",
+          "https://images.thrillophilia.com/image/upload/s--DuyEvZbn--/c_fill,h_600,q_auto,w_975/f_auto,fl_strip_profile/v1/images/photos/000/121/665/original/1510742977_treeek.jpg.jpg?1510742977",
        
        
-        name: "Magical Havelock ",
-        duration: "5 Nights / 6 Days",
+        name: "ChidiyaTapu ",
+        duration: "Port-Blair",
         description: "Port Blair – 3 Nights , Havelock – 2 Nights ",
       },
       {
         id: 3,
         imageLink:
-          "https://firebasestorage.googleapis.com/v0/b/tourismweb-3e2b8.appspot.com/o/havelocksingle.jpeg?alt=media&token=36bb5381-e237-4851-a360-67f8183ea206",
+          "https://thumbs.dreamstime.com/z/eco-tourism-trekking-evergreen-tropical-rain-forest-elephant-beach-havelock-island-andaman-islands-india-122695122.jpg",
        
        
-        name: "Magical Havelock ",
-        duration: "5 Nights / 6 Days",
+        name: "Elephant Beach",
+        duration: "Havelock, South Andaman",
         description: "Port Blair – 3 Nights , Havelock – 2 Nights ",
       },
       {
         id: 4,
         imageLink:
-          "https://firebasestorage.googleapis.com/v0/b/tourismweb-3e2b8.appspot.com/o/havelocksingle.jpeg?alt=media&token=36bb5381-e237-4851-a360-67f8183ea206",
+          "https://4.imimg.com/data4/KK/KK/GLADMIN-/images-sightlimestonecavesbaratang-500x500.jpg",
        
        
-        name: "Magical Havelock ",
-        duration: "5 Nights / 6 Days",
+        name: "Limestone Caves",
+        duration: "Baratang, Middle Andaman",
         description: "Port Blair – 3 Nights , Havelock – 2 Nights ",
       },
       {
         id: 5,
         imageLink:
-          "https://firebasestorage.googleapis.com/v0/b/tourismweb-3e2b8.appspot.com/o/havelocksingle.jpeg?alt=media&token=36bb5381-e237-4851-a360-67f8183ea206",
+          "https://cdn.experienceandamans.com/images/alfred-caves-diglipur-north-andaman.jpg",
        
        
-        name: "Magical Havelock ",
-        duration: "5 Nights / 6 Days",
+        name: "Alfred Caves",
+        duration: "Diglipur, North Andaman",
         description: "Port Blair – 3 Nights , Havelock – 2 Nights ",
       },
       {
         id: 6,
         imageLink:
-          "https://firebasestorage.googleapis.com/v0/b/tourismweb-3e2b8.appspot.com/o/havelocksingle.jpeg?alt=media&token=36bb5381-e237-4851-a360-67f8183ea206",
+          "https://static2.tripoto.com/media/filter/nl/img/172710/TripDocument/1508315657_sp.jpg",
        
        
-        name: "Magical Havelock ",
-        duration: "5 Nights / 6 Days",
+        name: "Saddle Peak",
+        duration: "Diglipur, North Andaman",
         description: "Port Blair – 3 Nights , Havelock – 2 Nights ",
       },
   ];
