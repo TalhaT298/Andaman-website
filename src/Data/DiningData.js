6f628985f249986b6333c4cb631238c6c1739e27
unknown <mvvinod777@gmail.com>
2022-11-18 15:12:19 +0530

    Dining Data Update

---
+++ src/Data/DiningData.js
@@ -0,0 +1,95 @@
+const datas = [
+  
+    {
+      id: 1,
+      imageLink:
+        "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/tan0s2nwzehtbsgnvvyv",
+     
+     
+      name: "Tandoor",
+      duration: "Tandoor, Indian",
+      description: "Port Blair – 3 Nights , Havelock – 2 Nights ",
+    },
+    {
+        id: 2,
+        imageLink:
+          "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/vldv4tvcmu3gz77yozqt",
+       
+       
+        name: "Trio",
+        duration: "Indian, Chinese",
+        description: "Port Blair – 3 Nights , Havelock – 2 Nights ",
+      },
+      {
+        id: 3,
+        imageLink:
+          "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/hlwnslijtgyvurubf66i",
+       
+       
+        name: "Tillai's Bakery and Sweets",
+        duration: "Bakery, Sweets",
+        description: "Port Blair – 3 Nights , Havelock – 2 Nights ",
+      },
+      {
+        id: 4,
+        imageLink:
+          "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/yryms99kxhdlgjbmxxvm",
+       
+       
+        name: "ICY SPICY VEG BAKERY & SWEETS",
+        duration: "Bakery, Desserts",
+        description: "Port Blair – 3 Nights , Havelock – 2 Nights ",
+      },
+      {
+        id: 5,
+        imageLink:
+          "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/udahj4hukhez49q5tsbf",
+       
+       
+        name: "Desi Lounge",
+        duration: "Snacks, Chinese",
+        description: "Port Blair – 3 Nights , Havelock – 2 Nights ",
+      },
+      {
+        id: 6,
+        imageLink:
+          "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/eofvypbo3quswj7c1k9s",
+       
+       
+        name: "Brewbeans Cafe",
+        duration: "Beverages, Pizzas",
+        description: "Port Blair – 3 Nights , Havelock – 2 Nights ",
+      },
+      {
+        id: 7,
+        imageLink:
+          "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/wdrqu0k0zingggrhe9pw",
+       
+       
+        name: "Chai Sutta Bar",
+        duration: "Beverages, Fast Food",
+        description: "Port Blair – 3 Nights , Havelock – 2 Nights ",
+      },
+      {
+        id: 8,
+        imageLink:
+          "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/qslyzfmay4vbga1v6zjt",
+       
+       
+        name: "Rooftop Cafe",
+        duration: "Cafe, Beverages",
+        description: "Port Blair – 3 Nights , Havelock – 2 Nights ",
+      },
+      {
+        id: 9,
+        imageLink:
+          "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/wcttac6kgff3falczvqw",
+       
+       
+        name: "CARDAMOM",
+        duration: "North Indian, Indian",
+        description: "Port Blair – 3 Nights , Havelock – 2 Nights ",
+      },
+  ];
+  export default datas;
+  
--- src/ServiceMenu/dinning.js
+++ src/ServiceMenu/dinning.js
@@ -1,274 +1,54 @@
+import { Grid ,Container} from "@nextui-org/react";
+import {motion} from "framer-motion"
+import DiningData from "../Data/DiningData";
+
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 
+const cardObject = DiningData.map(function(data) {
+  return (
+    <Grid.Container gap={2} justify="flex-start">
+    <Grid fluid>
+    <Card sx={{ display: 'flex' }}>
+      <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+          <CardMedia
+            component="img"
+            image={data.imageLink}
+            alt="Live from space album cover"
+            className="img-responsive card-img-top"
+            style={{ width: "auto", height: 179 }}
+          />
+        <CardContent sx={{ flex: '1 0 auto' }}>
+          <Typography component="div" variant="h5">
+            {data.name}
+          </Typography>
+          <Typography variant="subtitle1" color="text.secondary" component="div">
+            {data.duration}
+          </Typography>
+        </CardContent>
+        
+      </Box>
+    </Card>
+    </Grid>
+    </Grid.Container>
+
+  );
+});
+
 export default function dining() {
   
   return (
-    <div>
-    <Card sx={{ display: 'flex' }}>
-      <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-          <CardMedia
-            component="img"
-            image="https://i.pinimg.com/736x/2b/b3/d4/2bb3d46f16ee6abf842845949003857f.jpg"
-            alt="Live from space album cover"
-            className="img-responsive card-img-top"
-            style={{ width: "auto", height: 179 }}
-          />
-        <CardContent sx={{ flex: '1 0 auto' }}>
-          <Typography component="div" variant="h5">
-            Love Garden
-          </Typography>
-          <Typography variant="subtitle1" color="text.secondary" component="div">
-            
-          </Typography>
-        </CardContent>
-        
-      </Box>
-    </Card>
-    <Card sx={{ display: 'flex' }}>
-      <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-          <CardMedia
-            component="img"
-            image="https://i.pinimg.com/736x/2b/b3/d4/2bb3d46f16ee6abf842845949003857f.jpg"
-            alt="Live from space album cover"
-            className="img-responsive card-img-top"
-            style={{ width: "auto", height: 179 }}
-          />
-        <CardContent sx={{ flex: '1 0 auto' }}>
-          <Typography component="div" variant="h5">
-            Hotel Mansha Regency
-          </Typography>
-          <Typography variant="subtitle1" color="text.secondary" component="div">
-            
-          </Typography>
-        </CardContent>
-        
-      </Box>
-    </Card>
-    <Card sx={{ display: 'flex' }}>
-      <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-          <CardMedia
-            component="img"
-            image="https://i.pinimg.com/736x/2b/b3/d4/2bb3d46f16ee6abf842845949003857f.jpg"
-            alt="Live from space album cover"
-            className="img-responsive card-img-top"
-            style={{ width: "auto", height: 179 }}
-          />
-        <CardContent sx={{ flex: '1 0 auto' }}>
-          <Typography component="div" variant="h5">
-          Hotel Silver Spring
-          </Typography>
-          <Typography variant="subtitle1" color="text.secondary" component="div">
-            
-          </Typography>
-        </CardContent>
-        
-      </Box>
-    </Card>
-    <Card sx={{ display: 'flex' }}>
-      <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-          <CardMedia
-            component="img"
-            image="https://i.pinimg.com/736x/2b/b3/d4/2bb3d46f16ee6abf842845949003857f.jpg"
-            alt="Live from space album cover"
-            className="img-responsive card-img-top"
-            style={{ width: "auto", height: 179 }}
-          />
-        <CardContent sx={{ flex: '1 0 auto' }}>
-          <Typography component="div" variant="h5">
-            Hotel Ram Nivas
-          </Typography>
-          <Typography variant="subtitle1" color="text.secondary" component="div">
-        
-          </Typography>
-        </CardContent>
-        
-      </Box>
-    </Card>
-    <Card sx={{ display: 'flex' }}>
-      <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-          <CardMedia
-            component="img"
-            image="https://i.pinimg.com/736x/2b/b3/d4/2bb3d46f16ee6abf842845949003857f.jpg"
-            alt="Live from space album cover"
-            className="img-responsive card-img-top"
-            style={{ width: "auto", height: 179 }}
-          />
-        <CardContent sx={{ flex: '1 0 auto' }}>
-          <Typography component="div" variant="h5">
-            Sugar N Spice
-          </Typography>
-          <Typography variant="subtitle1" color="text.secondary" component="div">
-    
-          </Typography>
-        </CardContent>
-        
-      </Box>
-    </Card>
-    <Card sx={{ display: 'flex' }}>
-      <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-          <CardMedia
-            component="img"
-            image="https://i.pinimg.com/736x/2b/b3/d4/2bb3d46f16ee6abf842845949003857f.jpg"
-            alt="Live from space album cover"
-            className="img-responsive card-img-top"
-            style={{ width: "auto", height: 179 }}
-          />
-        <CardContent sx={{ flex: '1 0 auto' }}>
-          <Typography component="div" variant="h5">
-            Cardamom
-          </Typography>
-          <Typography variant="subtitle1" color="text.secondary" component="div">
-            
-          </Typography>
-        </CardContent>
-        
-      </Box>
-    </Card>
-    <Card sx={{ display: 'flex' }}>
-      <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-          <CardMedia
-            component="img"
-            image="https://i.pinimg.com/736x/2b/b3/d4/2bb3d46f16ee6abf842845949003857f.jpg"
-            alt="Live from space album cover"
-            className="img-responsive card-img-top"
-            style={{ width: "auto", height: 179 }}
-          />
-        <CardContent sx={{ flex: '1 0 auto' }}>
-          <Typography component="div" variant="h5">
-            Chai Sutta Bar
-          </Typography>
-          <Typography variant="subtitle1" color="text.secondary" component="div">
-            
-          </Typography>
-        </CardContent>
-        
-      </Box>
-    </Card>
-    <Card sx={{ display: 'flex' }}>
-      <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-          <CardMedia
-            component="img"
-            image="https://i.pinimg.com/736x/2b/b3/d4/2bb3d46f16ee6abf842845949003857f.jpg"
-            alt="Live from space album cover"
-            className="img-responsive card-img-top"
-            style={{ width: "auto", height: 179 }}
-          />
-        <CardContent sx={{ flex: '1 0 auto' }}>
-          <Typography component="div" variant="h5">
-            Hotel Shompen
-          </Typography>
-          <Typography variant="subtitle1" color="text.secondary" component="div">
-            
-          </Typography>
-        </CardContent>
-        
-      </Box>
-    </Card>
-    <Card sx={{ display: 'flex' }}>
-      <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-          <CardMedia
-            component="img"
-            image="https://i.pinimg.com/736x/2b/b3/d4/2bb3d46f16ee6abf842845949003857f.jpg"
-            alt="Live from space album cover"
-            className="img-responsive card-img-top"
-            style={{ width: "auto", height: 179 }}
-          />
-        <CardContent sx={{ flex: '1 0 auto' }}>
-          <Typography component="div" variant="h5">
-            Anumod Restaurant
-          </Typography>
-          <Typography variant="subtitle1" color="text.secondary" component="div">
-            
-          </Typography>
-        </CardContent>
-        
-      </Box>
-    </Card>
-    <Card sx={{ display: 'flex' }}>
-      <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-          <CardMedia
-            component="img"
-            image="https://i.pinimg.com/736x/2b/b3/d4/2bb3d46f16ee6abf842845949003857f.jpg"
-            alt="Live from space album cover"
-            className="img-responsive card-img-top"
-            style={{ width: "auto", height: 179 }}
-          />
-        <CardContent sx={{ flex: '1 0 auto' }}>
-          <Typography component="div" variant="h5">
-            The North Reef
-          </Typography>
-          <Typography variant="subtitle1" color="text.secondary" component="div">
-            
-          </Typography>
-        </CardContent>
-        
-      </Box>
-    </Card>
-    <Card sx={{ display: 'flex' }}>
-      <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-          <CardMedia
-            component="img"
-            image="https://i.pinimg.com/736x/2b/b3/d4/2bb3d46f16ee6abf842845949003857f.jpg"
-            alt="Live from space album cover"
-            className="img-responsive card-img-top"
-            style={{ width: "auto", height: 179 }}
-          />
-        <CardContent sx={{ flex: '1 0 auto' }}>
-          <Typography component="div" variant="h5">
-            Roof Top Cafe
-          </Typography>
-          <Typography variant="subtitle1" color="text.secondary" component="div">
-            
-          </Typography>
-        </CardContent>
-        
-      </Box>
-    </Card>
-    <Card sx={{ display: 'flex' }}>
-      <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-          <CardMedia
-            component="img"
-            image="https://i.pinimg.com/736x/2b/b3/d4/2bb3d46f16ee6abf842845949003857f.jpg"
-            alt="Live from space album cover"
-            className="img-responsive card-img-top"
-            style={{ width: "auto", height: 179 }}
-          />
-        <CardContent sx={{ flex: '1 0 auto' }}>
-          <Typography component="div" variant="h5">
-            Kalki
-          </Typography>
-          <Typography variant="subtitle1" color="text.secondary" component="div">
-            
-          </Typography>
-        </CardContent>
-        
-      </Box>
-    </Card>
-    <Card sx={{ display: 'flex' }}>
-      <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-          <CardMedia
-            component="img"
-            image="https://i.pinimg.com/736x/2b/b3/d4/2bb3d46f16ee6abf842845949003857f.jpg"
-            alt="Live from space album cover"
-            className="img-responsive card-img-top"
-            style={{ width: "auto", height: 179 }}
-          />
-        <CardContent sx={{ flex: '1 0 auto' }}>
-          <Typography component="div" variant="h5">
-            Desi Lounge
-          </Typography>
-          <Typography variant="subtitle1" color="text.secondary" component="div">
-            
-          </Typography>
-        </CardContent>
-        
-      </Box>
-    </Card>
+    <Container fluid>
+  
+     <motion.div initial={{x:'100vw'}} animate={{x:0}}  transition={{type:'spring',delay:0.7}}>
+   
+      {cardObject} 
+    </motion.div>
 
-    </div>
+    </Container>
+   
   );
 }
