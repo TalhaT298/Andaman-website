c104d6d911464f68714806cd20d1de922fb28242
im-paul-mak33 <souravpaul210789@gmail.com>
2023-01-06 04:46:06 +0530

    Added Lazy Loading

--- src/App.js
+++ src/App.js
@@ -1,39 +1,44 @@
+import { lazy, Suspense } from 'react';
+
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Explore from './NavMenu/Explore.js';
-import Fun from './NavMenu/Fun.js';
-import Stay from './NavMenu/Stay.js';
-import Flight from './ServiceMenu/Flights';
-import Ship from './ServiceMenu/Ship';
-import Hotel from './ServiceMenu/Hotel';
-import Cab from './ServiceMenu/Cab';
-import Bus from './ServiceMenu/Bus';
-import Activity from './ServiceMenu/Activity';
-import Pg from './ServiceMenu/Pg';
-import Travelpackage from './ServiceMenu/Travelpackage';
-import Dinning from './ServiceMenu/Dinning';
-import Trekking from './ServiceMenu/Trekking.js';
-import Nav from './Nav';
+
+const Explore = lazy(() => import('./NavMenu/Explore.js'));
+const Fun = lazy(() => import('./NavMenu/Fun.js'));
+const Stay = lazy(() => import('./NavMenu/Stay.js'));
+const Flights = lazy(() => import('./ServiceMenu/Flights'));
+const Ship = lazy(() => import('./ServiceMenu/Ship'));
+const Hotel = lazy(() => import('./ServiceMenu/Hotel'));
+const Cab = lazy(() => import('./ServiceMenu/Cab'));
+const Bus = lazy(() => import('./ServiceMenu/Bus'));
+const Activity = lazy(() => import('./ServiceMenu/Activity'));
+const Pg = lazy(() => import('./ServiceMenu/Pg'));
+const Travelpackage = lazy(() => import('./ServiceMenu/Travelpackage'));
+const Dinning = lazy(() => import('./ServiceMenu/Dinning'));
+const Trekking = lazy(() => import('./ServiceMenu/Trekking.js'));
+const Nav = lazy(() => import('./Nav'));
 
 export default function App() {
   return (
     <div className='bg-gradient-to-br from-indigo-900 via-pink-800 to-blue-800 text-white font-mono h-full m-0 p-0'>
       <Router>
-        <Routes>
-          <Route path='/' element={<Nav />} />
-          <Route path='/Explore' element={<Explore />} />
-          <Route path='/Stay' element={<Stay />} />
-          <Route path='/Fun' element={<Fun />} />
-          <Route path='/Flight' element={<Flight />} />
-          <Route path='/Ship' element={<Ship />} />
-          <Route path='/hotel' element={<Hotel />} />
-          <Route path='/bus' element={<Bus />} />
-          <Route path='/activity' element={<Activity />} />
-          <Route path='/pg' element={<Pg />} />
-          <Route path='/travelpg' element={<Travelpackage />} />
-          <Route path='/cab' element={<Cab />} />
-          <Route path='/dinning' element={<Dinning />} />
-          <Route path='/trekking' element={<Trekking />} />
-        </Routes>
+        <Suspense fallback={<h1> Loading.. .. ..... .</h1>}>
+          <Routes>
+            <Route path='/' element={<Nav />} />
+            <Route path='/Explore' element={<Explore />} />
+            <Route path='/Stay' element={<Stay />} />
+            <Route path='/Fun' element={<Fun />} />
+            <Route path='/Flight' element={<Flights />} />
+            <Route path='/Ship' element={<Ship />} />
+            <Route path='/hotel' element={<Hotel />} />
+            <Route path='/bus' element={<Bus />} />
+            <Route path='/activity' element={<Activity />} />
+            <Route path='/pg' element={<Pg />} />
+            <Route path='/travelpg' element={<Travelpackage />} />
+            <Route path='/cab' element={<Cab />} />
+            <Route path='/dinning' element={<Dinning />} />
+            <Route path='/trekking' element={<Trekking />} />
+          </Routes>
+        </Suspense>
       </Router>
     </div>
   );
