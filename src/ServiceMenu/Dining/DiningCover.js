d9e9bd9f0ac1559a0e11015f3c7ce5facd8143d6
im-paul-mak33 <souravpaul210789@gmail.com>
2023-01-15 20:22:13 +0530

    Redirect to Resto Info Page

--- src/ServiceMenu/Dining/DiningCover.js
+++ src/ServiceMenu/Dining/DiningCover.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
+import FoodAvailableHotel from './FoodAvailableHotel';
+import { Link } from 'react-router-dom';
 
 const DiningCover = (props) => {
   return (
     <div className='m-2 py-3 p-2 mx-auto flex flex-col w-88 xs:w-auto'>
-      <div className='flex flex-row xs:flex-col max-w-80 h-72 items:center'>
-        <a href={props.videoLink} target='_blank' rel='noreferrer'>
+      <Link to='foodavailablehotel'>
+        <div className='flex flex-row xs:flex-col max-w-80 h-72 items:center'>
           <LazyLoadImage
             src={props.imageLink}
             alt={props.name}
             className='flex rounded-md w-88 h-60 drop-shadow-2xl shadow-black shadow-lg transition:origin-center hover:ease-in-out hover:scale-110 transition:duration-1000 transition-transform'
           />
-        </a>
-      </div>
+        </div>
+      </Link>
       <div className='flex flex-col justify-between m-auto py-3 p-1 text-center'>
         <p className=''>{props.name}</p>
         <p>{props.cuisine}</p>
---
+++ src/ServiceMenu/Dining/FoodAvailableHotel.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FoodAvailableHotel = () => {
+  return (
+    <div className='w-full h-full'>
+      <div className='mx-auto'>My Name is Paul</div>
+    </div>
+  );
+};
+
+export default FoodAvailableHotel;
