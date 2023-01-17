e67a21d9eb1dc2ec5396388b6073d25fee38fdf3
im-paul-mak33 <souravpaul210789@gmail.com>
2023-01-18 00:18:12 +0530

    Future Component of food searching

--- src/ServiceMenu/Dining/FoodAvailableHotel.js
+++ src/ServiceMenu/Dining/FoodAvailableHotel.js
@@ -3,7 +3,7 @@ import React from 'react';
 const FoodAvailableHotel = () => {
   return (
     <div className='w-full h-full'>
-      <div className='mx-auto'>My Name is Paul</div>
+      <div className='mx-auto'>Food</div>
     </div>
   );
 };
