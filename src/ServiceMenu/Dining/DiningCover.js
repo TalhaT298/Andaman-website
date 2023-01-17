e2427729ecc979325b751c1611130eec313e221c
im-paul-mak33 <souravpaul210789@gmail.com>
2023-01-18 00:15:57 +0530

    Added Prebooking Function & Details

--- src/ServiceMenu/Dining/DiningCover.js
+++ src/ServiceMenu/Dining/DiningCover.js
@@ -1,27 +1,71 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
-import FoodAvailableHotel from './FoodAvailableHotel';
-import { Link } from 'react-router-dom';
+import Modal from 'react-modal';
+import Options from './Options';
 
-const DiningCover = (props) => {
+const TrekkingCover = (props) => {
+  const [openreserveInfo, setOpenReserveInfo] = useState(false);
+
   return (
-    <div className='m-2 py-3 p-2 mx-auto flex flex-col w-88 xs:w-auto'>
-      <Link to='foodavailablehotel'>
-        <div className='flex flex-row xs:flex-col max-w-80 h-72 items:center'>
-          <LazyLoadImage
-            src={props.imageLink}
-            alt={props.name}
-            className='flex rounded-md w-88 h-60 drop-shadow-2xl shadow-black shadow-lg transition:origin-center hover:ease-in-out hover:scale-110 transition:duration-1000 transition-transform'
-          />
-        </div>
-      </Link>
+    <div className='m-2 py-3 p-2 flex flex-col w-88'>
+      <div
+        className='max-w-80 items:center'
+        onClick={() => {
+          setOpenReserveInfo(!openreserveInfo);
+        }}
+      >
+        <LazyLoadImage
+          src={props.imageLink}
+          alt={props.name}
+          className='flex rounded-md w-88 h-64 drop-shadow-2xl shadow-black shadow-lg transition:origin-center hover:ease-in-out hover:scale-110 transition:duration-1000 transition-transform'
+        />
+      </div>
       <div className='flex flex-col justify-between m-auto py-3 p-1 text-center'>
         <p className=''>{props.name}</p>
-        <p>{props.cuisine}</p>
         <p>{props.location}</p>
       </div>
+      <Modal
+        isOpen={openreserveInfo}
+        onRequestClose={() => setOpenReserveInfo(false)}
+        overlayClassName='' // modal-overlay bg-black opacity
+        className='' // modal-content w-auto h-auto
+      >
+        <div className='flex flex-col w-auto h-auto font-Nunito_Sans bg-slate-50 shadow-2xl shadow-slate-200 my-10'>
+          <div className='mx-auto'>
+            <span className='text-3xl font-semibold text-slate-900'>
+              Reserve Your Table
+            </span>
+          </div>
+          <div className='flex flex-row justify-between pt-6 pb-5 mx-4'>
+            <span className='text-xl font-semibold text-black'>
+              {props.name}
+            </span>
+            <button
+              className='px-2 h-10 font-medium font-Nunito_Sans text-slate-400 hover:text-slate-800 text-2xl border-solid border-2'
+              onClick={() => setOpenReserveInfo(false)}
+            >
+              X
+            </button>
+          </div>
+
+          <div className='py-2 flex flex-wrap justify-evenly px-10'>
+            <LazyLoadImage
+              className='h-64 w-88 lg:w-5/12 airbnbml:w-2/3 airbnbml:mx-auto sm:h-52 sm:w-auto lexs:h-40 rounded-sm pb-3'
+              src={props.imageLink ? props.imageLink : '-'}
+              alt='.'
+            />
+            <Options />
+          </div>
+
+          <div className='flex flex-row pb-6 pt-10'>
+            <button className='mx-auto px-2 h-10 font-semibold font-Nunito_Sans text-white bg-gradient-to-br rounded-md to-amber-400 from-pink-500 hover:bg-gradient-to-br hover:to-amber-600 hover:from-pink-600 hover:text-white'>
+              Book Now
+            </button>
+          </div>
+        </div>
+      </Modal>
     </div>
   );
 };
 
-export default DiningCover;
+export default TrekkingCover;
