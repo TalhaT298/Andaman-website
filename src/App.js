6a25f97fb96b6cb980ed414bf57945db3f538d2f
im-paul-mak33 <souravpaul210789@gmail.com>
2023-02-13 01:18:56 +0530

    space

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { lazy, Suspense} from 'react';
+import { lazy, Suspense } from 'react';
 import {RingLoader, ScaleLoader} from 'react-spinners';
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
