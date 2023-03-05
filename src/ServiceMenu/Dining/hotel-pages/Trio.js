12d8153ef7b858be9ef7a8894828d6774e47d7d0
im-paul-mak33 <souravpaul210789@gmail.com>
2023-03-05 20:30:53 +0530

    made the pages mobile responsive, added modal

--- src/ServiceMenu/Dining/hotel-pages/Trio.js
+++ src/ServiceMenu/Dining/hotel-pages/Trio.js
@@ -16,58 +16,26 @@ const Trio = (props) => {
   return (
     <>
       <Navforwithout />
-      <div  className='bg-gradient-to-r from-slate-800 via-slate-600 to-slate-300 mt-6 w-10/12 mx-auto p-10 flex lg:flex-col justify-between'>
-        <div className='flex'>
-          <div className='w-60 mr-6'>
-           <img src={imageLink} alt="" />
+      <div className="bg-gradient-to-r from-slate-800 via-slate-600 to-slate-300 mt-6 w-10/12 mx-auto p-10 flex lg:flex-col justify-between">
+        <div className="flex xs:flex-col">
+          <div className="w-60 mr-6">
+            <LazyLoadImage src={imageLink} alt="" className="w-auto" />
           </div>
           <div>
-            <h1 className='text-white text-5xl'>{name}</h1>
-            <p className='text-gray-300 text-1xl mt-2'>{location}</p>
-            <p className='text-gray-300 text-1xl mt-2'>{speciality}</p>
+            <h1 className="text-white text-5xl">{name}</h1>
+            <p className="text-gray-300 text-base mt-2">{location}</p>
+            <p className="text-gray-300 text-base mt-2">{speciality}</p>
           </div>
         </div>
-        <div className='text-white pr-10 mt-10'>
-          <button onClick={!openreserveInfo} className='bg-slate-800 hover:bg-slate-300 px-5 py-4'>Check Out For More</button>
-        </div>
-      </div>
-      <div className='w-11/12 flex mx-auto mt-6'>
-        <div className='mt-10 mx-auto'>
-          <ul className='text-right'>
-            <li className='hover:text-orange-300'><Link>Best Food Available</Link></li>
-            <li className='hover:text-orange-300'><Link>Best Mixed Items</Link></li>
-            <li className='hover:text-orange-300'><Link>Best Fish Items</Link></li>
-          </ul>
-        </div>
-        <div className='mx-auto'>
-          <h3 className='font-bold text-2xl mb-3'>Best Food Available</h3>
-          <div className='flex justify-between items-center w-11/12 border p-7'>
-            <div className='text-center pl-10'>
-              <h2 className='font-bold text-green-500 text-2xl'>Vagi</h2>
-              <h2 className='font-bold'>420</h2>
-            </div>
-            <div className='w-1/5'>
-              <img src="https://images.unsplash.com/photo-1512621776951-a57141f2eefd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt="" />
-            </div>
-          </div>
-          <div className='flex justify-between items-center w-11/12 border p-7 mt-2'>
-            <div className='text-center pl-10'>
-              <h2 className='font-bold text-green-500 text-2xl'>Vagi</h2>
-              <h2 className='font-bold'>420</h2>
-            </div>
-            <div className='w-1/5'>
-              <img src="https://images.unsplash.com/photo-1512621776951-a57141f2eefd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt="" />
-            </div>
-          </div>
-          <div className='flex justify-between items-center w-11/12 border p-7 mt-2'>
-            <div className='text-center pl-10'>
-              <h2 className='font-bold text-green-500 text-2xl'>Vagi</h2>
-              <h2 className='font-bold'>420</h2>
-            </div>
-            <div className='w-1/5'>
-              <img src="https://images.unsplash.com/photo-1512621776951-a57141f2eefd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt="" />
-            </div>
-          </div>
+        <div className="text-white pr-10 mt-10">
+          <button
+            onClick={() => {
+              setOpenReserveInfo(true);
+            }}
+            className="bg-slate-800 hover:bg-slate-300 px-5 py-4"
+          >
+            Reserve Your Table
+          </button>
         </div>
       </div>
       <Modal
@@ -97,7 +65,7 @@ const Trio = (props) => {
           <div className="py-2 flex flex-wrap justify-evenly px-10 xs:flex-col">
             <LazyLoadImage
               className="h-64 w-auto lg:w-5/12 airbnbml:w-2/3 airbnbml:mx-auto sm:h-52 sm:w-auto lexs:h-40 lexs:w-auto rounded-sm pb-3"
-              src={props.imageLink ? props.imageLink : '-'}
+              src={imageLink ? imageLink : '-'}
               alt="."
             />
             <Options />
@@ -110,7 +78,63 @@ const Trio = (props) => {
           </div>
         </div>
       </Modal>
-    
+      <div className="w-10/12 flex mx-auto mt-6 xl:gap-4">
+        <div className="mt-10 mx-auto">
+          <ul className="text-right">
+            <li className="hover:text-orange-300 focus:text-teal-600">
+              <Link>Best Food Available</Link>
+            </li>
+            <li className="hover:text-orange-300 focus:text-teal-600">
+              <Link>Best Mixed Items</Link>
+            </li>
+            <li className="hover:text-orange-300 focus:text-teal-600">
+              <Link>Best Fish Items</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="mx-auto">
+          <h3 className="font-bold text-2xl mb-3">Best Food Available</h3>
+          <div className="flex justify-between items-center w-11/12 border p-7 lg:flex-col">
+            <div className="text-center">
+              <h2 className="font-bold text-green-500 text-2xl">Vagi</h2>
+              <h2 className="font-bold">420</h2>
+            </div>
+            <div className="w-2/6 h-auto">
+              <LazyLoadImage
+                src="https://images.unsplash.com/photo-1512621776951-a57141f2eefd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+                alt=""
+                className="w-full"
+              />
+            </div>
+          </div>
+          <div className="flex justify-between items-center w-11/12 border p-7 lg:flex-col mt-2">
+            <div className="text-center">
+              <h2 className="font-bold text-green-500 text-2xl">Vagi</h2>
+              <h2 className="font-bold">420</h2>
+            </div>
+            <div className="w-2/6 h-auto">
+              <LazyLoadImage
+                src="https://images.unsplash.com/photo-1512621776951-a57141f2eefd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+                alt=""
+                className="w-full"
+              />
+            </div>
+          </div>
+          <div className="flex justify-between items-center w-11/12 border p-7 lg:flex-col mt-2">
+            <div className="text-center">
+              <h2 className="font-bold text-green-500 text-2xl">Vagi</h2>
+              <h2 className="font-bold">420</h2>
+            </div>
+            <div className="w-2/6 h-auto">
+              <LazyLoadImage
+                src="https://images.unsplash.com/photo-1512621776951-a57141f2eefd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+                alt=""
+                className="w-full"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 };
