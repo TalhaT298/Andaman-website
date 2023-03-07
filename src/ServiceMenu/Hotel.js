aa7b230a739c01e443c59ed9d7dff96a7aa915e8
im-paul-mak33 <souravpaul210789@gmail.com>
2023-03-07 12:47:51 +0530

    added a new hotel info & removed slide images

--- src/ServiceMenu/Hotel.js
+++ src/ServiceMenu/Hotel.js
@@ -2,14 +2,15 @@ import React from 'react';
 import { useState } from 'react';
 import Lists from './Hotel/Cards/Lists.js';
 import Header from './Hotel/Header.js';
-import { list, list2 } from '../Data/Hotel-Section/cards-list';
+import { list } from '../Data/Hotel-Section/cards-list';
+// import { list2 } from '../Data/Hotel-Section/cards-list';
 
 const Hotel = () => {
   const [selectedFilter, setSelectedFilter] = useState(0);
   return (
     <div className='h-full w-auto font-Nunito_Sans text-md scroll-smooth'>
       <Header />
-      {selectedFilter === 0 ? <Lists list={list} /> : <Lists list={list2} />}
+      {selectedFilter === 0 ? <Lists list={list} /> : <Lists list={list} />}
     </div>
   );
 };
