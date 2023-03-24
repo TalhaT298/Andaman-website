8f38531ca633d15032b28343c787c16cac010aab
unknown <robsonkhzepflo@gmail.com>
2023-03-24 22:42:18 +0530

    added Hotel Reserve Contact Forms

--- src/Component/NewContactForm.js
+++ src/Component/NewContactForm.js
@@ -30,10 +30,10 @@ const ContactForm = () => {
     e.preventDefault();
     setStatus("Sending...");
 
-    await emailjs.sendForm('service_j38h615', 'template_im7qgz9', form.current, 'vVfAy9O62laKEC_Em')
+    await emailjs.sendForm('service_j38h615', 'template_i4kag1s', form.current, 'vVfAy9O62laKEC_Em')
       .then((result) => {
           console.log(result.text);
-          alert("Thanks for taking the time. The Sublime Island team will contact you soon.")
+          alert(result.text)
       }, (error) => {
           console.log(error.text);
           alert(error.text)
@@ -48,35 +48,26 @@ const ContactForm = () => {
     <form ref={form}
       className="w-[30%] sm:w-[60%] lg:w-[50%] xl:w-[40%] bg-white rounded-lg overflow-hidden shadow-xl border border-gray-300 p-10 md:p-6 lg:p-8" 
       onSubmit={handleSubmit}>
-      <h1 className="text-indigo-600 text-[26px] md:text-2xl font-bold mb-6 md:mb-3 tracking-wide">Contact Us</h1>
+      <h1 className="text-indigo-600 text-[26px] md:text-2xl font-bold mb-6 md:mb-3 tracking-wide">Get In Touch</h1>
       <div className="flex flex-col gap-1">
-        <input type="text" id="name" name="user_name" placeholder="Name"
+        <label htmlFor="name">Name</label>
+        <input type="text" id="name" name="user_name"
           className="h-[40px] outline-none p-4 rounded-md border border-gray-300 focus:border-2 focus:border-[rgba(0, 206, 158, 1)]" 
           required 
         />
       </div>
-      <div className="flex flex-col gap-1 mt-5">
-        <input type="email" id="email" name="user_email" placeholder="Email"
+      <div className="flex flex-col gap-1 mt-3">
+        <label htmlFor="email">Email</label>
+        <input type="email" id="email" name="user_email"
          className="h-[40px] outline-none p-4 rounded-md border border-gray-300 focus:border-2 focus:border-[rgba(0, 206, 158, 1)]" 
          required         
         />
       </div>
-      <div className="flex flex-col gap-1 mt-5">
-        <input type="number" id="phone" name="user_phone" placeholder="Phone number"
-         className="h-[45px] outline-none p-4 rounded-md border border-gray-300 focus:border-2 focus:border-[rgba(0, 206, 158, 1)]" 
-         required         
-        />
-      </div>
-      <div className="flex flex-col gap-1 mt-5">
-        <input type="datetime-local" id="arrival" name="user_arrival" placeholder="Expected Arrival"
-         className="h-[45px] font-thin outline-none p-4 rounded-md border border-gray-300 focus:border-2 focus:border-[rgba(0, 206, 158, 1)]" 
-         required         
-        />
-      </div>
-      <div className="flex flex-col gap-1 mt-5">
-        <textarea id="message" name="message" placeholder="Message"
-          className="h-[90px] outline-none p-3 rounded-md resize-none border border-gray-300 focus:border-2 focus:border-[rgba(0, 206, 158, 1)]" 
-          required
+      <div className="flex flex-col gap-1 mt-3">
+        <label htmlFor="message">Message</label>
+        <textarea id="message" name="message"
+        className="h-[90px] outline-none p-3 rounded-md resize-none border border-gray-300 focus:border-2 focus:border-[rgba(0, 206, 158, 1)]" 
+        required
         />
       </div>
       <button type="submit" 
