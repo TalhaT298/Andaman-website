6096e35e9825c4cc2da25a20ec84f69d0f1d717c
im-paul-mak33 <souravpaul210789@gmail.com>
2023-04-27 00:03:59 +0530

    removed uncessary ui elements

--- src/ServiceMenu/Pg.js
+++ src/ServiceMenu/Pg.js
@@ -3,14 +3,14 @@ import { useState } from 'react';
 import Lists from './Pg/Cards/Lists.js';
 // import Featured from './Pg/Featured.js';
 // import Filter from './Pg/Filter/Filter.js';
-import Header from './Pg/Header.js';
+// import Header from './Pg/Header.js';
 import { list, list2 } from '../Data/Pg-Section/cards-list';
 
 const Pg = () => {
   const [selectedFilter, setSelectedFilter] = useState(0);
   return (
     <div className='h-full w-auto font-Nunito_Sans text-md scroll-smooth'>
-      <Header />
+      {/* <Header /> */}
       {/* <Filter
         selectedFilter={selectedFilter}
         setSelectedFilter={setSelectedFilter}
