719029789d25cd15e32c56d4827d8f7e091edfa4
Ahammed Fahim Galib <galib0124@gmail.com>
2023-05-01 19:22:40 +0600

    accurate location on bambooflat done

--- src/ServiceMenu/Ferry/BambooflatChatham.js
+++ src/ServiceMenu/Ferry/BambooflatChatham.js
@@ -24,39 +24,41 @@ const BambooflatChatham = (props) => {
       snapshot.forEach((bamboochat) => {
         const newLat = bamboochat.val().latitude;
         const newLng = bamboochat.val().longitude;
-        smoothUpdateLat(newLat);
-        smoothUpdateLng(newLng);
+        setLat(newLat)
+        setLng(newLng)
+        // smoothUpdateLat(newLat);
+        // smoothUpdateLng(newLng);
       });
     });
     return () => {
       locationRef.off();
     };
   }, []);
-  const smoothUpdateLat = (newLat) => {
-    const latStep = (newLat - lat) / 10;
-    let currentLat = lat;
+  // const smoothUpdateLat = (newLat) => {
+  //   const latStep = (newLat - lat) / 10;
+  //   let currentLat = lat;
 
-    for (let i = 1; i <= 10; i++) {
-      // eslint-disable-next-line no-loop-func
-      setTimeout(() => {
-        currentLat += latStep;
-        setLat(currentLat);
-      }, i * 10);
-    }
-  };
+  //   for (let i = 1; i <= 10; i++) {
+  //     // eslint-disable-next-line no-loop-func
+  //     setTimeout(() => {
+  //       currentLat += latStep;
+  //       setLat(currentLat);
+  //     }, i * 10);
+  //   }
+  // };
 
-  const smoothUpdateLng = (newLng) => {
-    const lngStep = (newLng - lng) / 10;
-    let currentLng = lng;
+  // const smoothUpdateLng = (newLng) => {
+  //   const lngStep = (newLng - lng) / 10;
+  //   let currentLng = lng;
 
-    for (let i = 1; i <= 10; i++) {
-      // eslint-disable-next-line no-loop-func
-      setTimeout(() => {
-        currentLng += lngStep;
-        setLng(currentLng);
-      }, i * 10);
-    }
-  };
+  //   for (let i = 1; i <= 10; i++) {
+  //     // eslint-disable-next-line no-loop-func
+  //     setTimeout(() => {
+  //       currentLng += lngStep;
+  //       setLng(currentLng);
+  //     }, i * 10);
+  //   }
+  // };
 
   return (
     <div>
