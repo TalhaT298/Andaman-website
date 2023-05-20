d70f14d02bc6e446b211e1ef64260939b91b0af8
Fahim Galib <galib0124@gmail.com>
2023-05-20 18:44:13 +0600

    ship tab responsive complete

--- src/ServiceMenu/Ship/Shipoverview.js
+++ src/ServiceMenu/Ship/Shipoverview.js
@@ -6,41 +6,41 @@ import heart from '../../images/image_source_for_shiptab/heart.png';
 
 const Shipoverview = (props) => {
     return (
-        <div className='bg-white drop-shadow-xl rounded-lg flex px-10 py-5 mb-8'>
-            <div className='w-56 h-48 mr-8'>
+        <div className='bg-white drop-shadow-xl rounded-lg flex flex-row lg:flex-col px-10 py-5 mb-8 relative'>
+            <div className='w-56 h-48 lg:w-28 lg:h-10 lg:border lg:border-rose-400 mr-8'>
                 <img class="w-full h-full mr-3" src={props.flightLogo} alt="..." />
             </div>
             <div className='w-full mx-6'>
-                <div className='flex justify-between items-center'>
+                <div className='flex flex-row lg:flex-col lg:place-items-end lg:mt-[-2.6rem] justify-between items-center'>
                     <div className='flex items-center'>
-                        <h2 className='mr-4 border border-rose-400 px-2 py-1'>4.9</h2>
+                        <h2 className='mr-4 border border-rose-400 lg:rounded-lg px-2 py-1'>4.9</h2>
                         <h3 className='font-bold'>Very Good Review</h3>
                     </div>
                     <h2 className='text-sky-600 font-bold text-xl'>$7</h2>
                 </div>
                 <div className='flex justify-between items-center text-xl text-slate-500 mt-5'>
-                    <p>Wed, Dec 8</p>
-                    <p className="font-sans">{props.flightDuration}</p>
+                    <p className='lg:mt-[-13rem] lg:text-black lg:font-bold lg:ml-[-1.5rem] lg:mb-[-1rem]'>Wed, Dec 8</p>
+                    <p className="font-sans lg:absolute lg:top-44 lg:left-40 lg:text-black">{props.flightDuration}</p>
                 </div>
                 <div className='w-full flex items-center my-4'>
-                    <div className='border border-slate-600 w-5 h-5 mr-3'></div>
-                    <div className='w-full flex justify-between items-center'>
-                        <div className='flex items-center'>
-                            <p className='mr-4 font-bold text-2xl'>{props.departureTime}</p>
+                    <div className='border border-slate-600 w-5 h-5 mr-3 lg:hidden'></div>
+                    <div className='w-full flex justify-between items-center lg:ml-[-2rem]'>
+                        <div className='flex flex-row lg:flex-col items-center'>
+                            <p className='mr-4 font-bold text-2xl lg:text-lg lg:w-full'>{props.departureTime}</p>
                             <p className='text-xl text-slate-500'>{props.currentDestination}</p>
                         </div>
                         <div className='flex items-center'>
                             <img class="h-2 w-8 mx-3" src={lftarrow} alt="..." />
                             <img class="max-w-none mx-3" src={boat} alt="..." />
-                            <img class="h-2 w-8 mx-3" src={rhttarrow} alt="..." />
+                            <img class="h-2 w-8 lg:mr-5" src={rhttarrow} alt="..." />
                         </div>
-                        <div className='flex items-center'>
-                            <p className='mr-4 font-bold text-2xl'>{props.arrivalTime}</p>
+                        <div className='flex flex-row lg:flex-col  items-center lg:ml-8'>
+                            <p className='mr-4 font-bold text-2xl lg:text-lg lg:w-full'>{props.arrivalTime}</p>
                             <p className='text-xl text-slate-500'>{props.nextDestination}</p>
                         </div>
                     </div>
                 </div>
-                <div className='flex justify-end items-center mt-6'>
+                <div className='flex justify-end items-center mt-6 lg:hidden'>
                     <div className='border border-rose-400 px-2 py-2 mr-5'>
                         <img class="max-w-none" src={heart} alt="..." />
                     </div>
