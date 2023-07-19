43ebf950040bafc6ba6af0b5fdd0432809d58f23
Anik Barua <anikbb04@gmail.com>
2023-07-19 18:54:18 +0600

    fix responsive issue in firefox

--- src/ServiceMenu/WaterSports/WaterSportActivity/WaterSportActivity.js
+++ src/ServiceMenu/WaterSports/WaterSportActivity/WaterSportActivity.js
@@ -2,8 +2,9 @@ import React, { useContext, useEffect, useState } from 'react';
 import { WaterContext } from '../../../context/UseWaterContext';
 
 const WaterSportActivity = () => {
-    const { waterBookingInfo } = useContext(WaterContext)
+    const { waterBookingInfo, places } = useContext(WaterContext)
     // console.log(waterBookingInfo)
+    console.log(places)
     const [screenWidth, setScreenWidth] = useState(window.innerWidth);
 
     useEffect(() => {
@@ -18,17 +19,35 @@ const WaterSportActivity = () => {
         };
     }, [screenWidth]);
     const { iframeLink, fullDescription } = waterBookingInfo;
+
+    console.log(screenWidth)
     return (
-        <div className={`flex items-start justify-between gap-5 font-montserrat ${screenWidth < 1224 ? 'flex-col' : ''}`}>
-            <iframe
-                src={iframeLink}
-                title="Embedded Content"
-                className={`rounded-xl ${screenWidth < 1224 ? 'w-[75%] h-96 mx-auto' : 'w-10/12 h-72'}`}
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                allowFullScreen
-            ></iframe>
-            <p className='text-base text-black text-justify'>{fullDescription}</p>
-        </div>
+        <>
+            {
+                places ?
+                    <div className={`flex items-start justify-between gap-5 font-montserrat ${screenWidth < 1224 ? 'flex-col' : ''}`}>
+                        <iframe
+                            src={iframeLink}
+                            title="Embedded Content"
+                            className={`rounded-xl ${screenWidth < 1224 && screenWidth > 569 ? 'w-[75%] h-96 mx-auto' : 'h-72 mx-auto'} ${screenWidth < 568 ? 'w-full' : ''}`}
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                            allowFullScreen
+                        ></iframe>
+                        <p className='text-base text-black text-justify'>{fullDescription}</p>
+                    </div>
+                    :
+                    <div className={`flex items-start justify-between gap-5 font-montserrat flex-col`}>
+                        <iframe
+                            src={iframeLink}
+                            title="Embedded Content"
+                            className={`rounded-xl ${screenWidth < 1224 && screenWidth > 569 ? 'w-[75%] h-96 mx-auto' : 'w-10/12 h-72 mx-auto'} ${screenWidth < 568 ? 'w-full' : ''}`}
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                            allowFullScreen
+                        ></iframe>
+                        <p className='text-base text-black text-justify'>{fullDescription}</p>
+                    </div>
+            }
+        </>
     );
 };
 
